c00df03c8416a71cdeb82f9686db9a0248b2dd73
Doug Lardo <doug.lardo@gmail.com>
2023-12-02 12:22:27 -0800

    day2 p1 Gold Star

--- AoC2023/src/Day2.java
+++ AoC2023/src/Day2.java
@@ -3,14 +3,28 @@ import java.util.HashMap;
 
 public class Day2 {
     AoC aoc = new AoC();
-    ArrayList<String> data = aoc.getFile("data/day2sample.txt");
-
+    ArrayList<String> data = aoc.getFile("AoC2023/data/day2.txt");
+    int total = 0;
     public Day2() {
         for (int i = 0; i < data.size(); i++) {
+            int game_id = i + 1;
             String line = data.get(i);
             HashMap<String, Integer> game = getHighestPulls(line);
-            System.out.println("Highest Pulls for game " + i + ": " + game);
+            System.out.println("Highest Pulls for game " + game_id + ": " + game);
+            if (isValidGame(game)) total += game_id;
         }
+    }
+
+    private Boolean isValidGame(HashMap<String,Integer> game) {
+        if (!game.containsKey("red")) game.put("red", 0);
+        if (!game.containsKey("blue")) game.put("blue", 0);
+        if (!game.containsKey("green")) game.put("green", 0);
+
+        if(game.get("red") > 12) return false;
+        if(game.get("blue") > 14) return false;
+        if(game.get("green") > 13) return false;
+
+        return true;
     }
 
     private HashMap<String, Integer> getHighestPulls(String s) {
@@ -35,6 +49,6 @@ public class Day2 {
     }
 
     public int getResult() {
-        return -1;
+        return total;
     }
 }
