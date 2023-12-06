eea67f57b51a57759b64682949c733247d211404
Doug Lardo <doug.lardo@gmail.com>
2023-12-06 11:11:34 -0800

    hashmap for scores

--- AoC2023/src/Day4.java
+++ AoC2023/src/Day4.java
@@ -1,8 +1,12 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+
 
 public class Day4 {
   String day = "4";
@@ -10,13 +14,24 @@ public class Day4 {
   //ArrayList<String> data = aoc.getFile("data/day" + day + "sample.txt");
   ArrayList<String> data = aoc.getFile("data/day" + day + ".txt");
   double p1Points = 0;
+  HashMap<Integer, Double> scores = new HashMap<Integer, Double>();
 
   public Day4() {
     System.out.println("Happy Day " + day + "!");
     System.out.println("https://adventofcode.com/2023/day/" + day);
 
-    for (String line : data) {
+    scores = calcScoresForAllCards(data);
+    // calc total
+    for (Entry<Integer, Double> score : scores.entrySet()) {
+      p1Points += score.getValue();
+    }
+  }
 
+  public HashMap<Integer, Double> calcScoresForAllCards(ArrayList<String> data) {
+    HashMap<Integer, Double> result = new HashMap<Integer, Double>();
+    int card = 0;
+    for (String line : data) {
+      card = getCardNumber(line);
       System.out.print("Card " + getCardNumber(line) + ": ");
       Set<Integer> winners = getWinners(line);
       Set<Integer> yourNums = getYourNumbers(line);
@@ -35,9 +50,11 @@ public class Day4 {
         subtotal = 0;
       }
 
+      result.put(card, subtotal);
       System.out.println("Score: " + subtotal);
-      p1Points += subtotal;
     }
+
+    return result;
   }
 
   // public int getCardNumber(String data) {
