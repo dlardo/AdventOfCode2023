31d01bf1435432b2ff81e2f6ab44a38aa1f46634
Doug Lardo <doug.lardo@gmail.com>
2023-12-05 17:34:19 -0800

    combine day3 into one file

--- AoC2023/src/Day3.java
+++ AoC2023/src/Day3.java
@@ -1,8 +1,14 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+
+// grid is quadrant 1 x.y
+//  2.0   2.1   2.2
+//  1.0   1.1   1.2
+//  0.0   0.1   0.2
 
 public class Day3 {
   String day = "day3";
@@ -10,7 +16,7 @@ public class Day3 {
   //ArrayList<String> data = aoc.getFile("data/" + day + "sample.txt");
   ArrayList<String> data = aoc.getFile("data/" + day + ".txt");
   public static Character NULLCHAR = (char) 0;
-  Day3Grid grid = new Day3Grid();
+  ArrayList<ArrayList<Character>> grid = new ArrayList<ArrayList<Character>>();
   int partNumberSum = 0;
   long gearRatioSum = 0;
 
@@ -24,12 +30,12 @@ public class Day3 {
   private void calcValidParts() {
     boolean validPart = false;
     ArrayList<Character> num = new ArrayList<Character>();
-    for (int y = 0; y < grid.length(); y++) {
-      for (int x = 0; x < grid.width(); x++) {
-        char c = grid.getNodeValue(x, y);
-        if (grid.isDigit(c)) {
+    for (int y = 0; y < length(); y++) {
+      for (int x = 0; x < width(); x++) {
+        char c = getNodeValue(x, y);
+        if (isDigit(c)) {
           num.add(c);
-          if (grid.adjacentToSpecial(x, y)) {
+          if (adjacentToSpecial(x, y)) {
             validPart = true;
           }
         // number is finished / NaN
@@ -53,13 +59,13 @@ public class Day3 {
     // check every x,y, find numbers
     ArrayList<Character> num = new ArrayList<Character>();
     ArrayList<ArrayList<Integer>> starList = new ArrayList<ArrayList<Integer>>();
-    for (int y = 0; y < grid.length(); y++) {
-      for (int x = 0; x < grid.width(); x++) {
-        char c = grid.getNodeValue(x, y);
-        if (grid.isDigit(c)) {
+    for (int y = 0; y < length(); y++) {
+      for (int x = 0; x < width(); x++) {
+        char c = getNodeValue(x, y);
+        if (isDigit(c)) {
           num.add(c);
           // if number is next to a star, get the star's coords
-          for (ArrayList<Integer> coordSet : grid.getCoordsOfNeighboringStars(x,y)) {
+          for (ArrayList<Integer> coordSet : getCoordsOfNeighboringStars(x,y)) {
             // store "star coords": [completed number]
             starList.add(coordSet);
           }
@@ -104,7 +110,7 @@ public class Day3 {
     // Load data into grid
     for (int y = 0; y < data.size(); y++) {
       for (int x = 0; x < data.get(y).length(); x++) {
-        grid.addNode(x, y, data.get(y).charAt(x));
+        addNode(x, y, data.get(y).charAt(x));
       }
     }
   }
@@ -115,5 +121,162 @@ public class Day3 {
 
   public long getPart2Result() {
     return gearRatioSum;
+  }
+
+  public char getNeighborValue(int x, int y, CardinalDirection cd) {
+    int[] coords = getNeighborCoords(x, y, cd);
+    return getNodeValue(coords[0], coords[1]);
+  }
+
+  public int[] getNeighborCoords(int x, int y, CardinalDirection cd) {
+    switch (cd) {
+      case N:
+        return new int[] { x, y - 1 };
+      case S:
+        return new int[] { x, y + 1 };
+      case E:
+        return new int[] { x + 1, y };
+      case W:
+        return new int[] { x - 1, y };
+      case NE:
+        return new int[] { x - 1, y - 1 };
+      case NW:
+        return new int[] { x + 1, y - 1 };
+      case SE:
+        return new int[] { x + 1, y + 1 };
+      case SW:
+        return new int[] { x - 1, y + 1 };
+      default:
+        System.err.println("Invalid direction");
+        return new int[] { 0 };
+    }
+  }
+
+  public char getNodeValue(int x, int y) {
+    if ((y > grid.size() - 1) || (y < 0))
+      return NULLCHAR;
+    if ((x > grid.get(y).size() - 1) || (x < 0))
+      return NULLCHAR;
+    return grid.get(y).get(x);
+  }
+
+  public void addNode(int x, int y, char val) {
+    while (grid.size() < y + 1) {
+      grid.add(new ArrayList<Character>());
+    }
+
+    // Fill in with empties if needed
+    for (int i = grid.get(y).size(); i <= x; i++) {
+      grid.get(y).add(NULLCHAR);
+    }
+    grid.get(y).set(x, val);
+  }
+
+  // starting at x,y, return all chars in that direction until the end of the grid
+  public ArrayList<Character> traverseDirection(int x, int y, CardinalDirection cd) {
+    return traverseDirection(x, y, cd, Integer.MAX_VALUE);
+  }
+
+  // returns ArrayList of Chars adjacent to the starting point at x,y and direction indicated
+  public ArrayList<Character> traverseDirection(int x, int y, CardinalDirection cd, int limit) {
+    ArrayList<Character> result = new ArrayList<>();
+    int[] c = getNeighborCoords(x, y, cd);
+    while (getNodeValue(c[0], c[1]) != NULLCHAR && result.size() < limit) {
+      result.add(getNodeValue(c[0], c[1]));
+      c = getNeighborCoords(c[0], c[1], cd);
+    }
+    return result;
+  }
+
+  // returns list of Coordinates of stars chars
+  public ArrayList<ArrayList<Integer>> getStarCoords() {
+    ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+    for (int y = 0; y < grid.size(); y++) {
+      for (int x = 0; x < grid.get(y).size(); x++) {
+        char c = grid.get(y).get(x);
+        if (isStar(c)) {
+          result.add(new ArrayList<>(Arrays.asList(x, y)));
+        }
+      }
+    }
+    return result;
+  }
+
+  public boolean isSpecial(Character c) {
+    if ((c > 32 && c < 46) ||
+        (c == 47) ||
+        (c > 57 && c < 65) ||
+        (c > 90 && c < 97) ||
+        (c > 123 && c < 127)) {
+      return true;
+    }
+    return false;
+  }
+
+  public boolean isStar(Character c) {
+    return (c == '*');
+  }
+
+  public boolean isDigit(Character c) {
+    if ((c > 47 && c < 58))
+      return true;
+    return false;
+  }
+
+  public boolean adjacentToSpecial(int x, int y) {
+    boolean result = false;
+    for (CardinalDirection cd : CardinalDirection.values()) {
+      if (traverseDirection(x, y, cd).size() > 0) {
+        result = result || isSpecial(traverseDirection(x, y, cd, 1).get(0));
+      }
+    }
+    return result;
+  }
+
+  public boolean adjacentToStar(int x, int y) {
+    boolean result = false;
+    for (CardinalDirection cd : CardinalDirection.values()) {
+      if (traverseDirection(x, y, cd).size() > 0) {
+        result = result || isStar(traverseDirection(x, y, cd, 1).get(0));
+      }
+    }
+    return result;
+  }
+
+  public ArrayList<ArrayList<Integer>> getCoordsOfNeighboringStars(int x, int y) {
+    ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+    for (CardinalDirection cd : CardinalDirection.values()) {
+      ArrayList<Character> traverseResult = traverseDirection(x, y, cd, 1);
+      if (traverseResult.size() > 0 && isStar(traverseResult.get(0))) {
+        int[] neighxy = getNeighborCoords(x, y, cd);
+        // adding new (x,y) pair
+        result.add(new ArrayList<Integer>(Arrays.asList(neighxy[0], neighxy[1])));
+      }
+    }
+    if (result.size() > 1) {
+      System.out.println(x + ", " + y + " was next to multiple stars");
+    }
+    return result;
+  }
+
+  public ArrayList<Character> adjacentChars(int x, int y) {
+    ArrayList<Character> result = new ArrayList<>();
+    for (CardinalDirection cd : CardinalDirection.values()) {
+      if (traverseDirection(x, y, cd).size() > 0) {
+        char c = traverseDirection(x, y, cd, 1).get(0);
+        if (c != NULLCHAR)
+          result.add(c);
+      }
+    }
+    return result;
+  }
+
+  public int length() {
+    return grid.size();
+  }
+
+  public int width() {
+    if (length() == 0) return 0;
+    return grid.get(0).size();
   }
 }
